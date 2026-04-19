0e96a822ee80a52f3e975fd2304f89c0d7407117
Nicole <nicoleo.bca24@chanakyauniversity.edu.in>
2026-04-19 14:16:01 +0000

    remove pre-save hook from User model

--- lifeline/server/models/User.js
+++ lifeline/server/models/User.js
@@ -1,37 +1,27 @@
 import mongoose from 'mongoose'
-import bcrypt from 'bcryptjs'
 
 const userSchema = new mongoose.Schema({
   name:      { type: String, required: true, trim: true },
   email:     { type: String, required: true, unique: true, lowercase: true, trim: true },
-  password:  { type: String, required: true, minlength: 6 },
+  password:  { type: String, required: true },
   university:{ type: String, default: '' },
   isPeerSupporter: { type: Boolean, default: false },
   peerProfile: {
-    specialties: [String],
-    bio: String,
-    available: { type: Boolean, default: false },
-    rating: { type: Number, default: 0 },
+    specialties:  [String],
+    bio:          String,
+    available:    { type: Boolean, default: false },
+    rating:       { type: Number, default: 0 },
     sessionCount: { type: Number, default: 0 },
   },
-  circle:    [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  circle:      [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   silentWatch: { type: Boolean, default: true },
   lastCheckin: { type: Date },
 }, { timestamps: true })
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
-  this.password = await bcrypt.hash(this.password, 12)
-  next()
-})
-
-userSchema.methods.comparePassword = function (plain) {
-  return bcrypt.compare(plain, this.password)
-}
-
 userSchema.methods.toSafeObject = function () {
-  const { password, ...safe } = this.toObject()
-  return safe
+  const obj = this.toObject()
+  delete obj.password
+  return obj
 }
 
 export default mongoose.model('User', userSchema)
